baa9a7a94beabd5b5ef833726500bbbebc1e13f3
Paolo Barresi <paolobarresi@bbfactory.it>
2018-06-28 15:07:30 +0200

    Replace Ball's velocity property with direction and speed

--- scn/ball.gd
+++ scn/ball.gd
@@ -1,24 +1,25 @@
 extends Area2D
 
 
-var velocity = Vector2(132, 132)
+var direction = Vector2(-1,1).normalized()
+var speed = 100
 var screensize
 
 
 func _ready():
-	screensize = get_viewport().size
+	screensize = get_viewport_rect().size
 
 
 func _process(delta):
-	position += velocity * delta
+	position += direction * speed * delta
 	# TODO: replace x collision with point score
 	if position.x < 0 or position.x > screensize.x:
-		velocity.x *= -1
+		direction.x *= -1
 		position.x = clamp(position.x, 0, screensize.x)
 	if position.y < 0 or position.y > screensize.y:
-		velocity.y *= -1
+		direction.y *= -1
 		position.y = clamp(position.y, 0, screensize.y)
 
 
 func _on_Ball_body_entered(body):
-	velocity.x *= -1
+	direction.x *= -1
