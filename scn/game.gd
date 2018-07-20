6adc1f50b224995669d5fc27481d103a35dcc8a2
Paolo Barresi <paolobarresi@bbfactory.it>
2018-07-20 22:07:22 +0200

    Implement transition from menu to game and back again

--- scn/game.gd
+++ scn/game.gd
@@ -1,11 +1,27 @@
 extends Node
 
 
+signal paused
+
+
 func _ready():
 	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed('ui_cancel'):
+		pause()
 
 func _on_point_scored(side):
 	if side == "left":
 		$score_left.text = str(int($score_left.text) + 1)
 	if side == "right":
 		$score_right.text = str(int($score_right.text) + 1)
+
+func pause():
+	emit_signal('paused')
+
+func unpause():
+	pass
+
+func set(p1, p2):
+	pass
