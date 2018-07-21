c51d1dba7382949cfb93eac196533f3f3c18c51e
Paolo Barresi <paolobarresi@bbfactory.it>
2018-07-21 22:59:07 +0200

    Rework ui to allow to choose human or ai for p1 and p2

--- scn/game.gd
+++ scn/game.gd
@@ -5,7 +5,7 @@ signal paused
 
 
 func _ready():
-	pass
+	pause()
 
 func _process(delta):
 	if Input.is_action_just_pressed('ui_cancel'):
@@ -24,5 +24,14 @@ func pause():
 func unpause():
 	get_tree().paused = false
 
-func set(p1, p2):
-	pass
+func set_p1_ai(ai):
+	$Player_1.ai = ai
+
+func set_p2_ai(ai):
+	$Player_2.ai = ai
+
+func reset():
+	$Ball.position = Vector2(160, 90)
+	$Ball.direction = Vector2(-1, 0)
+	$Player_1.position.y = 91
+	$Player_2.position.y = 90
